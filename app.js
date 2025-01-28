228c5d22a6f389d737d4173eafcba6c4d7edcb22
https://github.com/Sobansaud <159770646+Sobansaud@users.noreply.github.com>
2025-01-29 00:06:21 +0500

    main

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
 import express from "express";
 import cors from 'cors';
 import cookieParser from "cookie-parser";
-import userrouter from './Routes/user.routes.js'
-import videorouter from './Routes/video.routes.js'
- 
- 
-const app=express()
+import withdrawalRoutes from './routes/withdrawalRoutes';
+import referralRoutes from './routes/referral';
+import referralsRoutes from './routes/referrals';
+import adminRoutes from './routes/adminRoutes';
+import userrouter from './Routes/user.routes.js';
+import videorouter from './Routes/video.routes.js';
 
-app.use(express.json())
-app.use(cors())
-app.use(express.urlencoded({extended:true}))
-app.use(cookieParser())
+const app = express();
 
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.use('/api/v1/user',userrouter)
-app.use('/api/v1/video',videorouter)
- 
- 
- 
+// Routes
+app.use('/api/withdrawal', withdrawalRoutes);
+app.use('/api/referral', referralRoutes);
+app.use('/api/referrals', referralsRoutes);
+app.use('/api/admin', adminRoutes); // Admin routes for admin-related tasks
+app.use('/api/v1/user', userrouter);
+app.use('/api/v1/video', videorouter);
 
+// Set up the port
+const port = process.env.PORT || 5002;
 
-export {app}
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+export { app };
